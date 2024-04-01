fea1cc3f61640da55197e7913fdee97217f2f9de
Сытник Геннадий <sgen2003@list.ru>
2024-04-01 16:37:49 +0300

    fix

--- Tasks/Task2/task2.go
+++ Tasks/Task2/task2.go
@@ -1,12 +1,18 @@
-// rune синоним int32
-// byte синоним uint8
+// rune синоним int32 значение 0
+// byte синоним uint8 значение 0
 package main
 
 import "fmt"
 
 var a uint8
+var b byte
+var c rune
+var d uint8
 
 func main() {
 
 	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
--- task1.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Моя первая программа на Go!")
-}
