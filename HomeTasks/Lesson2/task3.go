9fd7ea6b36afb81f61d83ad3d143e1a57671a694
Сытник Геннадий <sgen2003@list.ru>
2024-04-01 23:17:00 +0300

    12

--- HomeTasks/Lesson2/task3.go
+++ HomeTasks/Lesson2/task3.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	a := 16
 	b := 3
-	c := 16 / 3
-	fmt.Printf("Результат: %s, остаток от деления: %s, тип результата: %t", c, b, c)
+	c := a / b
+	a = a - c*b
+	fmt.Println(c)
+	fmt.Println(a)
+	fmt.Printf("Результат: %v, остаток от деления: %v, тип результата: %T", c, a, c)
 }
