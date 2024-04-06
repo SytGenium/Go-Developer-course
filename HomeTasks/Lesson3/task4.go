07824c3abf82ef79d4d61c9a1f4bcc050f19f502
Сытник Геннадий <sgen2003@list.ru>
2024-04-06 20:36:55 +0300

    l3t4

---
+++ HomeTasks/Lesson3/task4.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	myGlobalConst1 = 679085
+	myGlobalConst2
+	myGlobalConst3
+	myGlobalConst4
+	myGlobalConst5
+)
+
+func main() {
+	fmt.Println(myGlobalConst1, myGlobalConst2, myGlobalConst3, myGlobalConst4, myGlobalConst5)
+}
