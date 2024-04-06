0eb443e864eef806389b048e537762bb8ec9bb87
Сытник Геннадий <sgen2003@list.ru>
2024-04-06 20:27:17 +0300

    l3t3_1

--- HomeTasks/Lesson3/task3.go
+++ HomeTasks/Lesson3/task3.go
@@ -6,5 +6,5 @@ const myGlobalConst = 679085
 
 func main() {
 	const myGlobalConst = 1
-	fmt.Printf("Моя первая константа %d!", myFirstConst)
+	fmt.Printf("Моя первая константа %d!", myGlobalConst)
 }
