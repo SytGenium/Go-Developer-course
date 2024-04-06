e5e3c367ff4ed681e111e39b64971cc51c4de8f7
Сытник Геннадий <sgen2003@list.ru>
2024-04-06 21:00:52 +0300

    l3t7

---
+++ HomeTasks/Lesson3/task7.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		myLocalConst1 = 1 << iota
+		myLocalConst2
+		myLocalConst3
+		myLocalConst4
+		myLocalConst5
+	)
+	fmt.Println(myLocalConst1, myLocalConst2, myLocalConst3, myLocalConst4, myLocalConst5)
+}
