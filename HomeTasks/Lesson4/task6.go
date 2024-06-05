64e3c717e97e05a086e2befe215f0b9d76dd321c
Сытник Геннадий <sgen2003@list.ru>
2024-06-05 22:21:50 +0300

    l4t5

---
+++ HomeTasks/Lesson4/task6.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	recurs(23, 0, 1)
+}
+
+func recurs(a int, b int, c int) {
+	a--
+	fmt.Println(b)
+
+	if a == 0 {
+		return
+	}
+	recurs(a, c, b+c)
+}
