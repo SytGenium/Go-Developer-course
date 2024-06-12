631425be7b39196e686344eade838dae2aa8b0a2
Сытник Геннадий <sgen2003@list.ru>
2024-06-12 14:45:17 +0300

    l8t4

---
+++ HomeTasks/Lesson8/task4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	type animal struct{}
+	emptyanimal := animal{}
+
+	s1 := map[string]animal{}
+	s1["слон"] = emptyanimal
+	s1["бегемот"] = emptyanimal
+	s1["носорог"] = emptyanimal
+	s1["лев"] = emptyanimal
+	s1["выдра"] = emptyanimal
+	fmt.Println(s1)
+}
