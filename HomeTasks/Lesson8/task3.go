9f6a01ca0140c9294e91ecf5b0a5fefcd9ba75e5
Сытник Геннадий <sgen2003@list.ru>
2024-06-12 14:40:07 +0300

    l8t3

---
+++ HomeTasks/Lesson8/task3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	type animal struct{}
+	emptyanimal := animal{}
+
+	s1 := map[string]animal{}
+	s1["слон"] = emptyanimal
+	s1["бегемот"] = emptyanimal
+	s1["носорог"] = emptyanimal
+	s1["лев"] = emptyanimal
+	delete(s1, "бегемот")
+	fmt.Println(s1)
+}
